217b14680392280129f1f3560ea93943fbb0bf8a
waynie.wei <waynie.wei@anker-in.com>
2024-11-06 16:15:33 +0800

    refactor: v2.0.0 添加go-zero框架实现

--- server/config/config.go
+++ server/config/config.go
@@ -1,23 +1,65 @@
 package config
 
-import (
-	"github.com/ve-weiyi/ve-blog-golang/server/config/properties"
-)
-
 type Config struct {
-	System properties.System `mapstructure:"system" json:"system" yaml:"system"`
-	Mysql  properties.Mysql  `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
-	Redis  properties.Redis  `mapstructure:"redis" json:"redis" yaml:"redis"`
-	// 消息队列、邮件发送服务
-	RabbitMQ properties.RabbitMQ `mapstructure:"rabbitmq" json:"rabbitmq" yaml:"rabbitmq"`
-	Email    properties.Email    `mapstructure:"email" json:"email" yaml:"email"`
-	// 鉴权
-	JWT   properties.JWT   `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
-	Oauth properties.Oauth `mapstructure:"oauth" json:"oauth" yaml:"oauth"`
-	// 日志
-	Zap properties.Zap `mapstructure:"zap" json:"zap" yaml:"zap"`
-	// 文件上传
-	Upload  properties.Upload  `mapstructure:"upload" json:"upload" yaml:"upload"`
-	Captcha properties.Captcha `mapstructure:"captcha" json:"captcha" yaml:"captcha"`
-	ChatGPT properties.ChatGPT `mapstructure:"chatgpt" json:"chatgpt" yaml:"chatgpt"`
+	System   SystemConf   `json:"system" yaml:"system"`
+	JWT      JWTConf      `json:"jwt" yaml:"jwt"`
+	Zap      Zap          `json:"zap" yaml:"zap"`
+	Mysql    MysqlConf    `json:"mysql" yaml:"mysql"`
+	Redis    RedisConf    `json:"redis" yaml:"redis"`
+	RabbitMQ RabbitMQConf `json:"rabbitmq" yaml:"rabbitmq"`
+}
+
+// 系统配置
+type SystemConf struct {
+	Version     string `json:"version" yaml:"version"`           // 程序版本
+	Env         string `json:"env" yaml:"env"`                   // 运行环境
+	Port        int    `json:"port" yaml:"port"`                 // 运行端口
+	RuntimePath string `json:"runtime-path" yaml:"runtime-path"` // 运行时目录
+}
+
+// jwt鉴权
+type JWTConf struct {
+	SigningKey  string `json:"signing-key" yaml:"signing-key"`   // jwt签名
+	ExpiresTime string `json:"expires-time" yaml:"expires-time"` // 过期时间
+	Issuer      string `json:"issuer" yaml:"issuer"`             // 签发者
+	Type        string `json:"type" yaml:"type"`                 // 类型 例如：Bearer
+}
+
+// mysql数据库
+type MysqlConf struct {
+	Host     string `json:"host" yaml:"host"`
+	Port     string `json:"port" yaml:"port"`
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+	Dbname   string `json:"dbname" yaml:"dbname"`
+	Config   string `json:"config" yaml:"config"`
+}
+
+// redis缓存
+type RedisConf struct {
+	Host     string `json:"host" yaml:"host"` // 服务器地址:端口
+	Port     string `json:"port" yaml:"port"`
+	Password string `json:"password" yaml:"password"` // 密码
+	DB       int    `json:"db" yaml:"db"`             // redis的哪个数据库
+}
+
+// rabbitmq消息队列
+type RabbitMQConf struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Zap struct {
+	Mode           string `json:"mode" yaml:"mode"`                       // 模式
+	Format         string `json:"format" yaml:"format"`                   // 输出
+	Level          string `json:"level" yaml:"level"`                     // 级别
+	Prefix         string `json:"prefix" yaml:"prefix"`                   // 日志前缀
+	EncodeCaller   string `json:"encode-caller" yaml:"encode-caller"`     // 编码调用者
+	EncodeColorful bool   `json:"encode-colorful" yaml:"encode-colorful"` // 编码调用者
+
+	Filename string `json:"filename" yaml:"filename"`   // 日志文件名称
+	CacheDir string `json:"cache-dir" yaml:"cache-dir"` // 日志文件夹
+	MaxAge   int    `json:"max-age" yaml:"max-age"`     // 日志留存时间
 }
--- server/infra/middleware/limit_ip.go
+++ server/infra/middleware/limit_ip.go
@@ -1,16 +1,34 @@
 package middleware
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/cast"
+
+	"github.com/ve-weiyi/ve-blog-golang/kit/infra/apierr"
+	"github.com/ve-weiyi/ve-blog-golang/kit/infra/apierr/codex"
 )
 
-func LimitIP() gin.HandlerFunc {
+// IP限流
+func LimitIP(cache *redis.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ip := c.ClientIP()
+		key := c.ClientIP()
 
-		// 如果需要限制ip,可以在这里进行限制
+		v, err := cache.Get(context.Background(), key).Result()
+		if err != nil && v != "" {
+			cache.SetEx(context.Background(), key, 1, 300)
+		}
 
-		c.Set("ip_address", ip)
+		// 短时间内请求10次
+		if cast.ToInt(v) > 10 {
+			c.JSON(http.StatusOK, apierr.NewApiError(codex.CodeTooManyRequests, "操作频繁,请在5分钟后再试"))
+			c.Abort()
+			return
+		}
+
 		c.Next()
 	}
 }
